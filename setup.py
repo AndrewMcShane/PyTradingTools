0d5da412a7b478229672e64f9666879e4d383273
Andrew McShane <andrewjohnmcshane@gmail.com>
2020-10-29 15:12:38 -0400

    update setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 setup(
     name='pytradingtools',
     license='GPL 3.0',
-    version='0.5',
+    version='0.6',
     packages=find_packages(exclude=('test',)),
     include_package_data=True,
     long_description=open('README.md').read()
